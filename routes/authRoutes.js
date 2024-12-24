90c7c812aef7c921f5733e27d4330817521e3f8c
PRANJALJAVIA <pranjaljavia762@gmail.com>
2024-12-24 16:02:08 +0530

    fixed register error

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,5 @@
 const express = require('express');
-const User = require('../models/userModel');
-const Verification = require('../models/verificationModel');
+const verificationModel = require('../models/verificationModel');
 const responseFunction = require('../utils/responseFunction');
 const nodemailer = require('nodemailer');
 const dotenv = require('dotenv');
@@ -9,6 +8,7 @@ const router = express.Router();
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const authTokenHandler = require('../middlewares/checkAuthToken');
+const userModel = require('../models/userModel');
 
 const mailer = async (receieveremail, code) => {
     let transporter = nodemailer.createTransport({
@@ -52,11 +52,11 @@ router.post('/sendotp', async (req, res, next) => {
     }
 
     try{
-        await Verification.deleteMany({email: email});
-        const code = Math.floor
+        await verificationModel.deleteMany({email: email});
+        const code = Math.floor(100000 + Math.random() * 900000);
         const isSent = await mailer(email, code);
 
-        const newVerification = new Verification({
+        const newVerification = await verificationModel.create({
             email: email,
             code: code
         })
@@ -84,10 +84,9 @@ router.post('/register', async(req, res) => {
     if(password.length < 6){
         return responseFunction(res, 400, "Password should be atleast 6 charactres long", null, false);
     }
-
     try{
-        const user = await User.findOne({email: email});
-        const verificationQueue = await Verification.findOne({email: email});
+        let user = await userModel.findOne({email: email});
+        const verificationQueue = await verificationModel.findOne({email: email});
 
         if(user){
             return responseFunction(res, 400, "User already exists", null, false);
@@ -102,18 +101,18 @@ router.post('/register', async(req, res) => {
             return responseFunction(res, 400, "Invalid OTP", null, false);
         }
 
-        user = new User({
-            name,
-            email,
-            password,
-            role, 
+        user = await userModel.create({
+            name: name,
+            email: email,
+            password: password,
+            role: role
         });
 
         await user.save();
-        await Verification.deleteOne({email: email});
+        await verificationModel.deleteOne({email: email});
 
         const authToken = jwt.sign({userId: user._id}, process.env.JWT_SECRET_KEY, {expiresIn: '1d'});
-        const refreshToken = jwt.sign({userId: user._id}, process.env.JWT_REFRESH_SECRET_KEY, {expiresIn: '10d'});
+        const refreshToken = jwt.sign({userId: user._id}, process.env.JWT_REFRESH_SECERET_KEY, {expiresIn: '10d'});
 
         res.cookie('authToken', authToken, {httpOnly: true, secure: true, sameSite: 'none'});
         res.cookie('refreshToken', refreshToken, {httpOnly: true, secure: true, sameSite: 'none'});
@@ -122,14 +121,15 @@ router.post('/register', async(req, res) => {
         return responseFunction(res, 200, "Registered successfully", {user, authToken, refreshToken}, true);
     }
     catch(err){
-        return responseFunction(res, 200, "Internal Server Error", err, false);
+        console.log(err);
+        return responseFunction(res, 500, "Internal Server Error", err, false);
     }
 })
 
 router.post('/login', async (req, res, next) => {
     try{
         const {email, password} = req.body;
-        const user = await User.findOne({email: email});
+        const user = await userModel.findOne({email: email});
 
         if(!user){
             return responseFunction(res, 400, "Invalid Credentials", null, false);
@@ -165,7 +165,7 @@ router.get('/checklogin', authTokenHandler, async (req, res, next) => {
 
 router.get('/getuser', authTokenHandler, async (req, res, next) => {
     try {
-        const user = await User.findById(req.userId).select(-password);
+        const user = await userModel.findById(req.userId).select(-password);
 
         if(!user) {
             return responseFunction(res, 400, "User not found", null, false);
